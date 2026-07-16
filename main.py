58974c3572246782a9f05b6ceeae046a3fd82821
吳志傑 <jay36539762@gmail.com>
2026-07-16 11:06:21 +0800

    Fastapi二版

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 
-app = FastAPI(title="Hello API")
+app = FastAPI(title="FastAPI")
 
 
-@app.get("/")
-def read_root() -> dict[str, str]:
+@app.get("/", summary="Read Root")
+def read_root() -> RedirectResponse:
+    return RedirectResponse(url="/docs", status_code=307)
+
+
+@app.get("/hello", summary="Read Hello")
+def read_hello() -> dict[str, str]:
     return {"message": "Hello"}
+
+
+@app.get("/api", summary="Read Api")
+def read_api() -> dict[str, str]:
+    return {
+        "name": "fastapi-render-hello",
+        "docs": "/docs",
+        "openapi": "/openapi.json",
+    }
